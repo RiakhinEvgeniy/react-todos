e6c13d3d23fd1adb46f629f5d9d6d0dbee06fe2e
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2025-07-31 13:29:47 +0200

    Fetch. Request array data.

---
+++ src/components/Post.js
@@ -0,0 +1,14 @@
+import './post.css'
+function Post({ title, body, userId, id }) {
+    return (
+        <div className="post">
+            <h1>{title}</h1>
+            <p>{body}</p>
+            <p>USERID: {userId}</p>
+            <p>{id}</p>
+        </div>
+
+    )
+}
+
+export default Post;
