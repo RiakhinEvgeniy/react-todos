fa65c2dcc81f2cb4743a5dc66beebcd8c71f1147
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2025-07-31 22:13:51 +0200

    FINISH: Working version v2.0

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import TodoForm from './components/Todos/TodoForm';
 import TodoList from './components/Todos/TodoList';
+import TodosActions from './components/Todos/TodosActions';
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -12,7 +13,7 @@ function App() {
       text: text,
       isCompleted: false,
       id: uuidv4()
-    }    
+    }
     setTodos([...todos, newTodo])
   }
 
@@ -22,15 +23,39 @@ function App() {
 
   const toggleTodoHandle = (id) => {
     setTodos(todos.map((todo) => {
-      return todo.id === id ? {...todo, isCompleted: !todo.isCompleted} : {...todo}
+      return todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : { ...todo }
     }))
   }
+
+  const resetTodosHandler = () => {
+    setTodos([])
+  }
+
+  const deleteCompletedTodosHandler = () => {
+    setTodos(todos.filter((todo) => !todo.isCompleted))
+  }
+
+  let completedTodosCount = todos.filter((todo) => todo.isCompleted).length
+  
 
   return (
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler} />
-      <TodoList todos={todos} deleteTodo={deleteTodoHandler} toggleTodo={toggleTodoHandle} />
+
+      {!!todos.length > 0 && <TodosActions
+      completedTodosExist={!!completedTodosCount}
+        resetTodos={resetTodosHandler}
+        deleteCompletedTodos={deleteCompletedTodosHandler} />
+
+      }
+
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodoHandler}
+        toggleTodo={toggleTodoHandle} />
+
+        {completedTodosCount > 0 && <h2>{`You have cmpleted ${completedTodosCount} ${completedTodosCount > 1 ? 'todos' : 'todo'}`}</h2>}
     </div>
   );
 }
