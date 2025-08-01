533bdb94ac4c75cd33fd6ab8ffd2bcb6121d613e
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2025-08-01 21:02:07 +0200

    ADD: BrowseRouter. Routes. Route. Outlet.

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Contacts from './components/Contacts';
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
+import './App.css'
+import MainLayout from './layouts/MainLayout';
+
+function App() {
+
+  return <BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+        <Route index={true} element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </div>
+  </BrowserRouter>
+
+}
+
+export default App;
