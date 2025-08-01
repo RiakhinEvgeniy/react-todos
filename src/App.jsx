03d2dd7a80adbf5c9788a7a7f48e5abff9ae2512
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2025-08-01 21:16:23 +0200

    ADD: CSS active for NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<MainLayout />}>
-        <Route index={true} element={<Home />} />
+          <Route index={true} element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contacts" element={<Contacts />} />
           <Route path="*" element={<NotFound />} />
